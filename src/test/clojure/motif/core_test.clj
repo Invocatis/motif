323445be4f6fa19cabfc82dcea39691a1cec94e2
Invocatis <invocatis@gmail.com>
2019-01-07 11:08:54 -0500

    Added tests for the match macro, and set patterns

--- src/test/clojure/motif/core_test.clj
+++ src/test/clojure/motif/core_test.clj
@@ -25,6 +25,13 @@
     (is (matches? [pos? pos? neg?] [1 1 -1]))
     (is (not (matches? [] [nil])))))
 
+(deftest test-set
+  (testing "Sets"
+    (is (matches? #{1 2} 1))
+    (is (matches? #{1 2} [1 2]))
+    (is (not (matches? #{1 2} 3)))
+    (is (not (matches? #{1 2} [1 2 3])))))
+
 (deftest test-seq
   (testing "Seqs"
     (is (matches? '(1) [1 1 1]))
@@ -32,6 +39,15 @@
     (is (matches? (flatten (repeat (list pos? neg?))) [1 -1 1 -1 1 -1 1]))))
 
 (deftest test-regex
-  (testing "Regexs"
+  (testing "Regexes"
     (is (matches? #"\d*" "123"))
     (is (matches? #"\[(\d\s)*\d?\]" [1 2 3 4]))))
+
+(deftest match-macro
+  (testing "Match Macro"
+    (is
+      (match "Hello"
+        1 "Not this one"
+        [1 2 3] "Or this one"
+        #{1 2 3} "Not at all"
+        #"Hello" true))))
